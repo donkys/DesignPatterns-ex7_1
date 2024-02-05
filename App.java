7ca595397cbc4ae15ca49dd4af2643b484aac9cf
donkys <63050156@kmitl.ac.th>
2024-02-06 00:02:38 +0700

    ex_7-1

---
+++ App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        // IImage myImage = new Image("test.jpg");
+        
+        IImage myImage = new ImageProxy("test.jpg");
+        myImage.display();        
+    }
+}
---
+++ ImageProxy.java
@@ -0,0 +1,18 @@
+public class ImageProxy implements IImage {
+    private String fileName;
+    Image image = null;
+
+    public ImageProxy(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void display() {
+        if (image == null) {
+            image = new Image(fileName);
+        }
+        System.out.println("Displaying " + fileName);
+
+    }
+
+}
